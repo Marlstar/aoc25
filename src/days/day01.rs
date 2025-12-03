6433745441e3a3c91ff506634afb148977430ba3
Marley Reeves <marley@marlstar.com>
2025-12-03 11:28:39 +1000

    day 1 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -13,4 +13,32 @@ impl crate::Day for Day01 {
         }
         counter
     }
+
+    fn part2(&self) -> isize {
+        let mut counter = 0;
+        let mut pos = 50isize;
+        for instruction in &self.input {
+            let ipos = pos;
+            let coeff = match instruction.chars().next().unwrap() {
+                'L' => -1,
+                'R' => 1,
+                _ => panic!()
+            };
+            let mag = instruction[1..].parse::<isize>().unwrap();
+            let delta100 = if coeff > 0 { 100 - ipos }
+            else if ipos == 0 { 100 }
+            else { ipos };
+            let offset = mag - delta100;
+            let val = if offset >= 0 { 1 + offset/100 } else { 0 };
+            counter += val;
+
+            pos = (pos + mag * coeff) % 100;
+            if pos < 0 { pos += 100 }
+
+            if coeff > 0 {
+                eprintln!("{ipos} + {mag} => {pos} (+{val})")
+            } else { eprintln!("{ipos} - {mag} => {pos} (+{val})")}
+        }
+        counter
+    }
 }
