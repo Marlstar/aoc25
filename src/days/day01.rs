05456e1da494f5d6698db8662d0bcf4f18018a85
Marley Reeves <marley@marlstar.com>
2025-12-03 10:56:15 +1000

    day 1 part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,4 +1,16 @@
 crate::line_day!(Day01);
 impl crate::Day for Day01 {
-
+    fn part1(&self) -> isize {
+        let mut counter = 0;
+        let mut pos = 50isize;
+        for instruction in &self.input {
+            match instruction.chars().next().unwrap() {
+                'L' => pos -= instruction[1..].parse::<isize>().unwrap(),
+                'R' => pos += instruction[1..].parse::<isize>().unwrap(),
+                _ => panic!()
+            }
+            if pos % 100 == 0 { counter += 1 }
+        }
+        counter
+    }
 }
